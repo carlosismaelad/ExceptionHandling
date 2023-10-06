cc38e11f6f649debaa7163c3da661c6c2dc627fa
carlosismaelad <carlosismaelad@outlook.com>
2023-10-05 22:41:40 -0300

    Para posterior refatoração

---
+++ PersonalizedExceptions/Program.cs
@@ -0,0 +1,55 @@
+﻿using PersonalizedExceptions.Entities;
+
+namespace PersonalizedExceptions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-In date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            
+
+            if (checkIn > checkOut)
+            {
+                Console.WriteLine("Error: Check-Out date must be after Check0-In date.");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+                
+                
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: The reservation dates to update must be future dates.");
+                }
+                else if (checkIn > checkOut)
+                {
+                    Console.WriteLine("Error: Check-Out date must be after Check0-In date.");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+                
+            }
+
+
+        }
+    }
+}
